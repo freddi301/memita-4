832170b1746b34a5d9de28acef9237ae0e41b884
Frederik Batuna <gobi301@gmail.com>
2026-02-07 19:12:47 +0100

    add: bare-kit

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,28 @@
-import { Text, View } from "react-native";
+import b4a from "b4a";
+import { useEffect, useState } from "react";
+import { Text } from "react-native";
+import { Worklet } from "react-native-bare-kit";
 
 export default function Index() {
-  return (
-    <View>
-      <Text>Hello world</Text>
-    </View>
-  );
+  const [response, setResponse] = useState<string | null>(null);
+
+  useEffect(() => {
+    const worklet = new Worklet();
+
+    const source = `
+    const { IPC } = BareKit
+
+    IPC.on('data', (data) => console.log(data.toString()))
+    IPC.write(Buffer.from('Hello from Bare!'))
+    `;
+
+    worklet.start("/app.js", source);
+
+    const { IPC } = worklet;
+
+    IPC.on("data", (data: Uint8Array) => setResponse(b4a.toString(data)));
+    IPC.write(b4a.from("Hello from React Native!"));
+  }, []);
+
+  return <Text>{response}</Text>;
 }
