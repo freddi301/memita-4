bdeee383ee3c672472558c076b0c4f1b2411998b
Frederik Batuna <gobi301@gmail.com>
2026-02-11 02:35:04 +0100

    make queries always suspend

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,15 @@ import { SelectAccountScreen } from "@/components/screens/SelectAccountScreen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "../components/Routing";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+      refetchOnMount: "always",
+      gcTime: 0,
+    },
+  },
+});
 
 export default function Index() {
   return (
