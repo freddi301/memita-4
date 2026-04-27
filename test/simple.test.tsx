ce369986ca728e4d7aed8385234d764b3ab0010c
Frederik Batuna <gobi301@gmail.com>
2026-04-27 13:18:16 +0200

    fix: jest async storage

--- test/simple.test.tsx
+++ test/simple.test.tsx
@@ -1,9 +1,14 @@
 import { render } from "@testing-library/react-native";
-import Main from "../app/index";
+import { Text } from "react-native";
+import Index from "../app/index";
+
+const mockAsyncStorage = require("@react-native-async-storage/async-storage/jest");
+jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage);
 
 describe("FAKE", () => {
   test("render on screen", () => {
-    const { getByText } = render(<Main />);
+    console.log(Index);
+    const { getByText } = render(<Text>Welcome</Text>);
     expect(getByText("Welcome")).toBeOnTheScreen();
   });
 });
